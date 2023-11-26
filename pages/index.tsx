df1cce002344691387796aad013d8d239116a4b5
R3v180 <123840995+R3v180@users.noreply.github.com>
2023-11-26 08:03:04 +0100

    Commit from Stub AI (#1)
    Co-authored-by: R3v180 <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,49 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from 'react';
+import Question, { QuestionProps } from '../components/Question';
+import Scoreboard from '../components/Scoreboard';
+import GameMode from '../components/GameMode';
 
-const inter = Inter({ subsets: ['latin'] })
+interface QuestionData extends Omit<QuestionProps, 'onAnswer'> {}
 
-export default function Home() {
+const Home: React.FC = () => {
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+  const [currentQuestion, setCurrentQuestion] = useState<QuestionData | null>(null);
+  const [score, setScore] = useState(0);
+  const [gameMode, setGameMode] = useState('easy');
+
+  useEffect(() => {
+    // Fetch questions from public/questions.json
+    fetch('/questions.json')
+      .then(response => response.json())
+      .then(data => setQuestions(data[gameMode]));
+  }, [gameMode]);
+
+  const handleAnswer = (answer: string) => {
+    if (currentQuestion && answer === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+    }
+    const nextQuestion = questions.pop();
+    setCurrentQuestion(nextQuestion ? nextQuestion : null);
+  };
+
+  const handleModeSelect = (mode: string) => {
+    setGameMode(mode);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-    </main>
-  )
-}
+    <div>
+      <GameMode onModeSelect={handleModeSelect} />
+      {currentQuestion && (
+        <Question
+          question={currentQuestion.question}
+          options={currentQuestion.options}
+          correctAnswer={currentQuestion.correctAnswer}
+          onAnswer={handleAnswer}
+        />
+      )}
+      <Scoreboard score={score} />
+    </div>
+  );
+};
+
+export default Home;
